47ff4ff985b113e95ac73fd748017b3c81ae043c
snow188 <houssouathanase1@gmail.com>
2024-06-07 12:46:25 +0100

    Create Uptime2.js

---
+++ scripts/cmds/Uptime2.js
@@ -0,0 +1,39 @@
+module.exports = {
+  config: {
+    name: "uptime2",
+    aliases: ["upt2",],
+    version: "1.0",
+    author: "OtinXSandip",
+    role: 0,
+    shortDescription: {
+      en: "Displays the total number of users of the bot and check uptime "
+    },
+    longDescription: {
+      en: "Displays the total number of users who have interacted with the bot and check uptime."
+    },
+    category: "system",
+    guide: {
+      en: "Use {p}totalusers to display the total number of users of the bot and check uptime."
+    }
+  },
+  onStart: async function ({ api, event, args, usersData, threadsData }) {
+    try {
+      const uptime = process.uptime();
+      
+      const days = Math.floor(uptime / 86400);
+      const hours = Math.floor((uptime % 86400) / 3600);
+      const minutes = Math.floor((uptime % 3600) / 60);
+      const seconds = Math.floor(uptime % 60);
+      
+      const uptimeString = `➪${days} ❄️Days❄️, 
+➪${hours} ❄️Hours❄️, 
+➪${minutes} ❄️Minutes❄️, 
+➪${seconds} ❄️Seconds❄️`;
+      
+      api.sendMessage(`👑 |Time| 👑 ....\n${uptimeString}`, event.threadID);
+    } catch (error) {
+      console.error(error);
+      api.sendMessage("An error occurred while retrieving data.", event.threadID);
+    }
+  }
+}
