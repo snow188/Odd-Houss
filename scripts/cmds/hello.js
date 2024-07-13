5577b8bfac0bd6866d0f9da375d29df066c593de
snow188 <houssouathanase1@gmail.com>
2024-07-13 12:43:27 +0100

    Update hello.js

--- scripts/cmds/hello.js
+++ scripts/cmds/hello.js
@@ -1,34 +1,32 @@
-module.exports = {
-  config: {
-    name: "hello",
-    version: "1.0",
-    author: "odiamus",
-      countDown: 5,
-    role: 2 ,
-    shortDescription: {
-      en: "this is short description of cmd"
-    },
-    longDescription: {
-      en: "this is long description of cmd"
-    },
-    category: "owner",
-    guide:{
-      en:"this is guide of cmd" 
-    }
-  },
-  
-   lang: {
-     en: {
-       hello: "hello_world",
-         helloWithName: "hello your facebook id is %1" } // English language
-   },
+ module.exports = {
+ config: {
+ name: "hello", 
+ version: "1.0", 
+ author: "odiamus", 
+ countDown: 5, 
+ role: 2, 
+ shortDescription: {
+ en: "this is short description of command"
+ }, 
+ longDescription: {
+ en: "this is long description of command"
+ },
+ category: "categoryName",
+ guide: {
+ en: "this is guide of command"
+ } 
+ },
+
+ lang: {
+ en: {
+ hello: "hello_world",
+ helloWithName: "hello, your facebook id is %1"
+ } // English language
+ },
 
 
-  onStart: async function ({ api, args, message, event, threadsData, usersData, dashBoardData, globalData, threadModel, userModel, dashBoardModel, globalModel, role, commandName, getLang })
-  { message.reply("hello my friends world")
-   }
+ onStart: async function ({ api, args, message, event, threadsData, usersData, dashBoardData, globalData, threadModel, userModel, dashBoardModel, globalModel, role, commandName, getLang }) {
+ message.reply("hello my friends world")
+
+ }
 };
-  
-      
-      
-    
