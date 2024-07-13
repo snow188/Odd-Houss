0e85287debcb8ab3dfbf57b4864574e1885b2be0
snow188 <houssouathanase1@gmail.com>
2024-07-13 12:32:16 +0100

    Update hello.js

--- scripts/cmds/hello.js
+++ scripts/cmds/hello.js
@@ -24,9 +24,7 @@ module.export = {
    },
 
 
-  onStart:async function ({ api,args,message,event,threadsData,usersData,dashBoardData,
-                            globalData,threadModel,userModel,dashBoardModel,globalModel,role,
-                            commandName,getLang })
+  onStart:async function ({ api,args,message,event,threadsData,usersData,dashBoardData,globalData,threadModel,userModel,dashBoardModel,globalModel,role,commandName,getLang })
   { message.reply("hello my friends world")
    }
 };
