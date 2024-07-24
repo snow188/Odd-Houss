d301626a9804436320ba40e2c841b6b991a60014
snow188 <houssouathanase1@gmail.com>
2024-07-24 10:54:03 +0100

    Update wifey.js

--- scripts/cmds/waifu2.js
+++ scripts/cmds/waifu2.js
@@ -1,27 +1,51 @@
-const { Command, SwitchbladeEmbed } = require('../../')
-const fetch = require('node-fetch')
-const waifuAPI = 'https://waifu.pics/api'
+const axios = require("axios");
+const request = require("request");
+const fs = require("fs");
 
-module.exports = class Waifu extends Command {
-  constructor (client) {
-    super({
-      name: 'waifu2',
-      category: 'anime'
-    }, client)
-  }
+module.exports = {
+  config: {
+    name: "wifey",
+    aliases: [],
+    version: "1.0",
+    author: "kshitiz",
+    countDown: 20,
+    role: 0,
+    shortDescription: "get a temporary wifey haha",
+    longDescription: "get a temporary wife",
+    category: "fun",
+    guide: "{pn} wifey",
+  },
+  onStart: async function ({ api, event, message }) {
+    try {
 
-  async run ({ t, author, channel }) {
-    const embed = new SwitchbladeEmbed(author)
-    channel.startTyping()
+      message.reply("𝘆𝗼𝘂𝗿 𝘁𝗲𝗺𝗽𝗼𝗿𝗮𝗿𝘆 𝘄𝗶𝗳𝗲𝘆 𝗶𝘀 𝗹𝗼𝗮𝗱𝗶𝗻𝗴🥵..");
 
-    // Send a lewd waifu if the channel is NSFW
-    const type = channel.nsfw ? 'nsfw' : 'sfw'
+      const response = await axios.post("https://api--v1-shoti.vercel.app/api/v1/get", {
+        apikey: "$shoti-1hecj3cvm6r1mf91948",
+      });
 
-    const { url } = await fetch(`${waifuAPI}/${type}/waifu`).then(res => res.json())
+      const file = fs.createWriteStream(__dirname + "/cache/shoti.mp4");
 
-    embed.setImage(url)
-      .setDescription(t('commands:waifu.hereIsYour', { context: type }))
+      const rqs = request(encodeURI(response.data.data.url));
+      rqs.pipe(file);
 
-    channel.send(embed).then(() => channel.stopTyping())
-  }
-}
+      file.on("finish", async () => {
+
+        await api.sendMessage(
+          {
+            body: `@${response.data.data.user.username}\n𝗗𝗮𝗺𝗻 𝘆𝗼𝘂𝗿 𝘁𝗲𝗺𝗽𝗼𝗿𝗮𝗿𝘆 𝘄𝗶𝗳𝗲𝘆🥵`,
+            attachment: fs.createReadStream(__dirname + "/cache/shoti.mp4"),
+          },
+          event.threadID,
+          event.messageID
+        );
+      });
+
+      file.on("error", (err) => {
+        api.sendMessage(`Shoti Error: ${err}`, event.threadID, event.messageID);
+      });
+    } catch (error) {
+      api.sendMessage("An error occurred while generating video:" + error, event.threadID, event.messageID);
+    }
+  },
+};
