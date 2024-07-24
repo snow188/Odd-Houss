50ca2c6d6781f835d0bdbd0f452b284ee1630ed6
snow188 <houssouathanase1@gmail.com>
2024-07-24 10:50:05 +0100

    Update imgur.js

--- scripts/cmds/imgur.js
+++ scripts/cmds/imgur.js
@@ -1,37 +1,38 @@
 const axios = require('axios');
 
 module.exports = {
-		config: {
-				name: "imgur",
-				version: "1.0.0",
-				role: 0,
-				author: "eugene",
-				shortDescription: "imgur upload",
-				countDown: 0,
-				category: "imgur",
-				guide: {
-						en: '[reply to image]'
-				}
-		},
+ config: {
+ name: "imgur",
+ aliases: ["Imgur"],
+ version: "1.0",
+ author: "Shinpei",
+ countDown: 5,
+ role: 0,
+ shortDescription: {
+ en: "Upload image to imgur"
+ },
+ longDescription: {
+ en: "Upload image to imgur by replying to photo"
+ },
+ category: "image",
+ guide: {
+ en: ""
+ }
+ },
 
-		onStart: async ({ api, event }) => {
-				let link2;
+ onStart: async function ({ api, event }) {
+ const linkanh = event.messageReply?.attachments[0]?.url;
+ if (!linkanh) {
+ return api.sendMessage('Please reply to an image.', event.threadID, event.messageID);
+ }
 
-				if (event.type === "message_reply" && event.messageReply.attachments.length > 0) {
-						link2 = event.messageReply.attachments[0].url;
-				} else if (event.attachments.length > 0) {
-						link2 = event.attachments[0].url;
-				} else {
-						return api.sendMessage('No attachment detected. Please reply to an image.', event.threadID, event.messageID);
-				}
-
-				try {
-						const res = await axios.get(`http://fi3.bot-hosting.net:20284/imgur?link=${encodeURIComponent(link2)}`);
-						const link = res.data.uploaded.image;
-						return api.sendMessage(`Here is the Imgur link for the image you provided:\n\n${link}`, event.threadID, event.messageID);
-				} catch (error) {
-						console.error("Error uploading image to Imgur:", error);
-						return api.sendMessage("An error occurred while uploading the image to Imgur.", event.threadID, event.messageID);
-				}
-		}
+ try {
+ const res = await axios.get(`https://main-api.cat0tom2.repl.co/imgur?link=${encodeURIComponent(linkanh)}`);
+ const juswa = res.data.uploaded.image;
+ return api.sendMessage(juswa, event.threadID, event.messageID);
+ } catch (error) {
+ console.log(error);
+ return api.sendMessage('Failed to upload image to imgur.', event.threadID, event.messageID);
+ }
+ }
 };
